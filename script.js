2673281eda83ca86b8328742c95d93b123268b0f
Jescklay Matos <jescklaysoft@hotmail.com>
2024-05-10 20:27:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function clickMenu() {
+  document.getElementById("burguer").classList.toggle("bi-x");
+  document.getElementById("burguer").classList.toggle("bi-list");
+
+  if (itens.style.display == "block") {
+    itens.style.display = "none";
+  } else {
+    itens.style.display = "block";
+  }
+}
+
+function abrirHtml() {
+  document.getElementById("html").style.display = "block";
+}
+
+function esconderHtml() {
+  document.getElementById("html").style.display = "none";
+}
+
+function abrirCss() {
+  document.getElementById("css").style.display = "block";
+}
+
+function esconderCss() {
+  document.getElementById("css").style.display = "none";
+}
+
+function abrirJavascript() {
+  document.getElementById("javascript").style.display = "block";
+}
+
+function esconderJavascript() {
+  document.getElementById("javascript").style.display = "none";
+}
+
+function abrirSql() {
+  document.getElementById("sql").style.display = "block";
+}
+
+function esconderSql() {
+  document.getElementById("sql").style.display = "none";
+}
+
+function abrirProjetos() {
+  document.getElementById("mostrar-projetos").style.display = "block";
+}
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const elements = document.querySelectorAll(".hidden");
+elements.forEach((Element) => myObserver.observe(Element));
